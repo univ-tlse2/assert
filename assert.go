489abcb137de57a14db493b325a36089e7b3020a
jaco60 <jacoboni@univ-tlse2.fr>
2021-05-12 14:12:49 +0200

    Add recover to assert function

--- assert.go
+++ assert.go
@@ -5,18 +5,17 @@ import "fmt"
 
 // Assert vérifie que le test réussit ou panique et affiche le message dans le cas contraire
 func Assert(test bool, messOk, messNok string) {
-	defer noMessages()
+	defer func() {
+		if r := recover(); r != nil {
+			fmt.Println("Échec :", r)
+		}
+	}()
+
 	if !test {
 		panic(messNok)
 	} else {
 		if messOk != "" {
 			fmt.Println("Succès :", messOk)
 		}
-	}
-}
-
-func noMessages() {
-	if r := recover(); r != nil {
-		fmt.Println("Échec :", r)
 	}
 }
